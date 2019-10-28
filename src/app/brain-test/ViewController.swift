98012e8a07a0e6d34f30f9e9129e0b3296750227
Quang minh Dinh <qdinh@asb9816-02.csil.sfu.ca>
2019-10-28 16:42:18 -0700

    Adding app/brain-test for the brain test feature on app

---
+++ src/app/brain-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  brain-test
+//
+//  Created by Quang minh Dinh on 10/28/19.
+//  Copyright © 2019 Lucky13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
