e870fe1df40246050adc71dfd63c5fa8cb08f1c1
Angus Kan <Angusk@sfu.ca>
2019-11-01 14:32:51 -0700

    implemented screen shift-up in response to keyboard popup

--- src/PAPER Planes/MessagingViewController.swift
+++ src/PAPER Planes/MessagingViewController.swift
@@ -9,19 +9,47 @@
 import UIKit
 
 class MessagingViewController: UIViewController, UITextFieldDelegate {
+    // MARK: Properties
+    @IBOutlet weak var MsgTxtField: UITextField!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    
+        
+        // Handle the text field's  inputs through delegate callbacks
+        MsgTxtField.delegate = self
+        
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object:nil )
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
+   
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        view.endEditing(true)
+    }
+    
+    // MARK: Actions
+    @IBAction func MsgTxtField(_ sender: Any) {
+        
+    }
+    
     
     deinit {
-        // Stop listening to keyboard hide/show events
+         //Stop listening to keyboard hide/show events
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
@@ -31,13 +59,13 @@ class MessagingViewController: UIViewController, UITextFieldDelegate {
     
     @objc func keyboardWillChange(notification: Notification){
         print("Keyboard will show:\(notification.name.rawValue) ")
-        
+
         guard let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
             return
         }
-        
+
         view.frame.origin.y = -keyboardRect.height
-        
+
     }
     
     
