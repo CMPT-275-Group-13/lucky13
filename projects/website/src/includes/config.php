177a5fa5ef0a2b287f1cf1e1132d9150c4e19a71
Choong Jin Ng <jin8383@gmail.com>
2019-11-01 15:06:46 -0700

    Added Google Firebase scripts

--- projects/website/src/includes/config.php
+++ projects/website/src/includes/config.php
@@ -1,32 +1,16 @@
 <?php
-	switch ($_SERVER["SCRIPT_NAME"]) {
-		case "/php-template/about.php":
-			$CURRENT_PAGE = "Paper Plane | About"; 
-			$PAGE_TITLE = "About Us";
-			break;
-		case "/php-template/contact.php":
-			$CURRENT_PAGE = "Paper Plane | Contact"; 
-			$PAGE_TITLE = "Contact Us";
-			break;
-		default:
-			$CURRENT_PAGE = "Paper Plane";
-			$PAGE_TITLE = "Welcome to my homepage!";
-	}
-
-	/**
-	 * Server stuff
-	 */
-	$servername = "localhost";
-	$username = "root"; // Not intended for production use
-	$password = "root";
-
-	// Create connection
-	$conn = new mysqli($servername, $username, $password);
-
-	// Check connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-
+switch ($_SERVER["SCRIPT_NAME"]) {
+	case "/php-template/about.php":
+		$CURRENT_PAGE = "Paper Plane | About"; 
+		$PAGE_TITLE = "About Us";
+		break;
+	case "/php-template/contact.php":
+		$CURRENT_PAGE = "Paper Plane | Contact"; 
+		$PAGE_TITLE = "Contact Us";
+		break;
+	default:
+		$CURRENT_PAGE = "Paper Plane";
+		$PAGE_TITLE = "Welcome to my homepage!";
+}
 
 ?>
