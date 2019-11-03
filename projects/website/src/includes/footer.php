5ae83637110ac8ea2265344049b09ebb6e9a09fd
Choong Jin Ng <jin8383@gmail.com>
2019-11-03 01:28:36 -0800

    Improved footer.php

--- projects/website/src/includes/footer.php
+++ projects/website/src/includes/footer.php
@@ -1,3 +1,6 @@
-<div class="footer">
-	&copy; <?php print date("Y");?>
+<div id="footer">
+	<div class="" id="footer-line">
+		<span id="site-map"><a href="">Site Map</a></span>
+		<span id="copyright">&copy; <?php print date("Y");?> CMPT 275 Lucky13</span>
+	</div>
 </div>
