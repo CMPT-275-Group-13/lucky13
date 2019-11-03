bdd668380a022d7af0bc1db295adfe8cfc2b9718
Elaine Crisostomo <ecrisost@sfu.ca>
2019-11-03 13:30:48 -0800

    Add backend functionality for doctor profile and patient profile. Added ids to index, profile elements

--- src/lucky13/config.js
+++ src/lucky13/config.js
@@ -11,54 +11,25 @@
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-var firestore = firebase.firestore();
 
-const docRef = firestore.doc("/doctors/soz17cW7ZfzeEgyny3h3");
-const patientRef = firestore.doc("/patient/rgUuJGiHo6PWnktwd9PW");
-const patient = document.querySelector("#patient1");
-const patient2 = document.querySelector("#patient2");
-const saveButton = document.querySelector("#saveButton");
 
-// saveButton.addEventListener("click", function(){
-//     const textToSave = inputTextField.value;
-//     console.log("I am going to save " + textToSave + " to FireStore");
-//     //set the collection so we don't have to worry about it
-//     docRef.set({
-//         hotDogStatus: textToSave  
-//     }).then(function(){
-//         console.log("Status saved!");
-//     }).catch(function(error){
-//         console.log("Got an error: ", error);
-//     });
-// })
+// buttonSignIn.addEventListener("click", function() {
+//     // console.log(username.value);
+//     // const docRef = firestore.doc("/doctors/jdoe");
 
-// loadButton.addEventListener("click", function(){
-//     docRef.get().then(function(doc){
-//         if(doc && doc.exists){
-//             const myData = doc.data();
-//             outputHeader.innerText = "Hotdog status: " + myData.hotDogStatus;
-//         }
-//     }).catch(function(error) {
-//         console.log("Got an error: ", error);
-//     });
+    
+//     // docRef.get().then(function(doc){
+//     //     if(doc && doc.exists){
+//     //         const myData = doc.data();
+//     //         Console.log(doc.firstName);
+//     //     }
+//     // });
+
+//     if(username.value === "jdoe" && password.value === "t3st0rder"){
+//         console.log("Good Job");
+//         location.href = "profile.html";
+//     }
 // });
 
-getRealtimeUpdates = function(){
-    docRef.onSnapshot(function(doc){
-        if(doc && doc.exists) {
-            const myData = doc.data();
-            //const myPatient = firestore.doc("/patient/tuUosuerNtfNzOCksRj7");
-            //console.log("Patient Name: " + myData.patient[0]);
-            patient.innerText = "Name: " + myData.patient[0];
-        }
-    });
 
-    patientRef.onSnapshot(function(doc){
-        if(doc && doc.exists){{
-            const myData = doc.data();
-            patient2.innerText = "Email: " + myData.emailAddress + "\n" + "Phone Number: " + myData.phoneNumber;
-            
-        }}
-    });
-}
-getRealtimeUpdates();
+
