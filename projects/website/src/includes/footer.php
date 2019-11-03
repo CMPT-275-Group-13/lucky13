ab09681e4cb4d9d728ea0a9f8f02d23f584acf07
Choong Jin Ng <jin8383@gmail.com>
2019-11-03 01:49:12 -0800

    Added class "footer" to footer.php

--- projects/website/src/includes/footer.php
+++ projects/website/src/includes/footer.php
@@ -1,4 +1,4 @@
-<div id="footer">
+<div class="footer" id="footer">
 	<div class="" id="footer-line">
 		<span id="site-map"><a href="">Site Map</a></span>
 		<span id="copyright">&copy; <?php print date("Y");?> CMPT 275 Lucky13</span>
