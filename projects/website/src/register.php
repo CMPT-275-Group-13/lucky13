680e6e48f3d70cf0801e033b3648e29487ef8076
Choong Jin Ng <jin8383@gmail.com>
2019-11-12 02:36:46 -0800

    Created account registration

--- projects/website/src/register.php
+++ projects/website/src/register.php
@@ -1,56 +1,41 @@
 <?php include("includes/config.php");?>
 
-<?php
-
-$username = '';
-$password = '';
-
-if (isset($_POST) && $_SERVER["REQUEST_METHOD"] == "POST") {
-	if (isset($_POST['username']) && isset($_POST['password'])) {
-		$username = sanitiseUserInput($_POST['username']);
-		$password = sanitiseUserInput($_POST['password']);
-		//  $password = hashPassword($password);
-
-		// TO-DO: Check User authentication
-		// If valid, redirect to landing page
-		if (true) {
-			redirect('landing.php'); // Do we want index.php to be the landing page?
-		}
-	}
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<?php include("includes/head-tag-contents.php");?>
+	<script type="text/javascript" src="includes/javascript/account-register.js"></script>
+	<script type="text/javascript">
+		checkUserAuthentication();
+	</script>
 </head>
 <body>
 
 <?php include("includes/design-top.php");?>
-<?php include("includes/navigation.php");?>
+<?php //include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
 	<div>
 		<div class="" id="">
-			<form action="" method="POST">
-				<div>
+			<div class="" id="register-status">
+			</div>
+			<form id="register-form" class="form-inline" action="" method="POST">
+				<div class="form-group">
 					<div>
-						<span>Email Address</span>
+						<label><b>Email</b></label>
 					</div>
 					<div>
-						<input type="textbox" name="Email Address"/>
+						<input class="form-control" type="textbox" name="email" id="email" required/>
 					</div>
 				</div>
 				<div>
-					<span>Password</span>
+					<label><b>Password</b></label>
 				</div>
 				<div>
-					<input type="textbox" name="Password"/>
+					<input class="form-control" type="password" name="password" id="password" required/>
 				</div>
-				<div>
-					<input type="submit" name="submit" value="Register"/>
+				<div class="form-group">
+					<button id="register-btn" class="btn btn-default">Register</button>
 				</div>
 			</form>
 		</div>
