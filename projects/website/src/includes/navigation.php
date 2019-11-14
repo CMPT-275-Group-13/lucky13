f16777ec08861641972a1f252ba08e587e9d1376
Choong Jin Ng <jin8383@gmail.com>
2019-11-14 15:37:21 -0800

    Moved about.php nav item to footer

--- projects/website/src/includes/navigation.php
+++ projects/website/src/includes/navigation.php
@@ -4,9 +4,6 @@
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="index.php" target="">Home</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "About") {?>active<?php }?>" href="about.php" target="_blank">About Us</a>
-	  </li>
-	  <li class="nav-item">
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Profile") {?>active<?php }?>" href="profile.php" target="">Profile</a>
 	  </li>
 	  <li class="nav-item">
