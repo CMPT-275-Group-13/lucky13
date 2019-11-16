48d620559b97ebea9da491bc9ccf06ac50852396
Choong Jin Ng <jin8383@gmail.com>
2019-11-16 03:13:33 -0800

    Fixed navigation links

--- projects/website/src/includes/navigation.php
+++ projects/website/src/includes/navigation.php
@@ -7,21 +7,18 @@
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Profile") {?>active<?php }?>" href="profile.php" target="">Profile</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Patient") {?>active<?php }?>" href="search.php" target="">Patient</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Patient") {?>active<?php }?>" href="search.php" target="">Search</a>
 	  </li>
-	  <?php if (isset($_GET) && isset($_GET['patient-id'])): ?>
-	  <?php $patientID = sanitiseInput($_GET['patient-id']); ?>
 	  <li class="nav-item">
 	  	<?php $patientProfile = "patient-profile.php/";?>
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Patient Profile") {?>active<?php }?>" href="patient-profile.php" target="">Profile</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Patient Profile") {?>active<?php }?>" href="patient-profile.php" target="">Patient</a>
 	  </li>
 	  <li class="nav-item">
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Patient Messaging") {?>active<?php }?>" href="message.php" target="">Message</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Patient Tests") {?>active<?php }?>" href="tests.php" target="">Message</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Patient Tests") {?>active<?php }?>" href="tests.php" target="">Tests</a>
 	  </li>
-	  <?php endif; ?>
 	  <li class="nav item">
 	  	<a class="nav-link" onclick="firebaseAccountSignOut()" target="">Sign Out</a>
 	  </li>
