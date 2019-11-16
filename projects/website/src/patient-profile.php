e00de6e762da44df2ddaa3980b0965d176e7e922
Choong Jin Ng <jin8383@gmail.com>
2019-11-15 21:07:32 -0800

    Removed unnecessary code

--- projects/website/src/patient-profile.php
+++ projects/website/src/patient-profile.php
@@ -1,24 +1,6 @@
 <?php include("includes/config.php");?>
 <?php require_once("includes/helper.php");?>
 
-<?php
-
-// if (isset($_GET) && isset($_GET['patient-id'])) {
-// 	$patientID = sanitiseUserInput($_GET['patient-id']);
-
-// 	// TO-DO: From $patientID, get the patient information from database
-
-
-// 	if (false) {
-// 		// Unable to find requested patient-id.
-// 		// Redirect back to search page
-// 	}
-// }
-
-?>
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -28,8 +10,6 @@
 
 <?php include("includes/design-top.php");?>
 <?php include("includes/navigation.php");?>
-
-<?php // var_dump($_GET); ?>
 
 <div class="container" id="main-content">
 	<div>
