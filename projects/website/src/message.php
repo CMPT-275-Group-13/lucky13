117242af76afc41495e57dff99837b877076d858
Choong Jin Ng <jin8383@gmail.com>
2019-11-16 03:14:07 -0800

    Added messaging text input

--- projects/website/src/message.php
+++ projects/website/src/message.php
@@ -11,9 +11,26 @@
 <?php include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
+	<div>
+		<div></div>
+	</div>
+	<div>
+		<div>
+			<form action="" method="POST">
+				<div>
+					<input type="textarea" name="message"/>
+				</div>
+				<div>
+					<button id="message-btn" class="btn btn-default">Send</button>
+				</div>
+			</form>
+		</div>
+	</div>
 </div>
 
 <?php include("includes/footer.php");?>
+
+<script src="includes/javascript/message.js" text="text/javascript"></script>
 
 </body>
 </html>
