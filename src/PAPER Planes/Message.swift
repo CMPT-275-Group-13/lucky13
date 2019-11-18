4bc2a0b7030907185323a0f57480576a643d873c
marco <marcoliang333@gmail.com>
2019-11-18 15:24:23 -0800

    Added ability to send messages

--- src/PAPER Planes/Message.swift
+++ src/PAPER Planes/Message.swift
@@ -9,36 +9,42 @@
 import Foundation
 import UIKit
 import MessageKit
+import Firebase
+import FirebaseFirestore
 
 struct Member {
 //TODO: Add UID
-  let name: String
+    let name: String
+    let email: String
 }
 
-struct Message {
-    let member: Member
+struct Message: MessageType {
+//    var messageId: String
+    var sender: SenderType
+    var sentDate: Date
+    
+//    let member: Member
     let text: String
     let id: String?
-    let sentDate: Date
+//    let sentDate: Date
 //    let sender: Sender
     
     init(member: Member, text: String){
-        self.member = member
+//        member = member
         self.text = text
+        sender = Sender(id: member.email, displayName: member.name)
         sentDate = Date()
+//        messageId = UUID().uuidString
         id = nil
     }
     
-}
-
-extension Message: MessageType {
     var messageId: String{
-        return UUID().uuidString
+        return id ?? UUID().uuidString
     }
     
-    var sender: SenderType {
-      return Sender(id: member.name, displayName: member.name)
-    }
+//    var sender: SenderType {
+//      return Sender(id: member.email, displayName: member.name)
+//    }
 
 //    var sentDate: Date {
 //        return Date()
@@ -48,10 +54,67 @@ extension Message: MessageType {
         return .text(text)
     }
     
+//    var downloadURL: URL? = nil
+
+    init?(document: QueryDocumentSnapshot) {
+        let data = document.data()
+      
+    guard let sentDate = data["sentDate"] as? Date else {
+        print("1")
+        return nil
+    }
+    guard let senderID = data["author"] as? String else {
+        print("2")
+        return nil
+    }
+    guard let displayName = data["authorName"] as? String else{
+        print("3")
+        return nil
+    }
+      
+      id = document.documentID
+      
+      self.sentDate = sentDate
+      sender = Sender(id: senderID, displayName: displayName)
+        
+        print("4")
+      if let body = data["body"] as? String {
+        self.text = body
+//        downloadURL = nil
+//      } else if let urlString = data["url"] as? String, let url = URL(string: urlString) {
+//        downloadURL = url
+//        content = ""
+      } else {
+        return nil
+      }
+        
+    }
+    
 }
 
 extension Message: Equatable{
     static func == (lhs: Message, rhs: Message) -> Bool {
         return lhs.messageId == rhs.messageId
     }
+}
+
+
+extension Message: DatabaseRepresentation {
+  
+  var representation: [String : Any] {
+    let rep: [String : Any] = [
+        "author": sender.senderId,
+        "sentDate": sentDate,
+        "body": text
+    ]
+//
+//    if let url = downloadURL {
+//      rep["url"] = url.absoluteString
+//    } else {
+//      rep["body"] = text
+//    }
+    
+    return rep
+  }
+  
 }
