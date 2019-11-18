16e1011fcbcd9f07018f18a3f78e7ea7a013cffa
Choong Jin Ng <jin8383@gmail.com>
2019-11-18 14:48:54 -0800

    Added links to tests.php

--- projects/website/src/tests.php
+++ projects/website/src/tests.php
@@ -11,6 +11,12 @@
 <?php include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
+	<div>
+		<a href="brain-test-results.php"><p>B.R.A.I.N Test Result</p></a>
+	</div>
+	<div>
+		<a href=""><p>Tremor Test Result</p></a>
+	</div>
 </div>
 
 <?php include("includes/footer.php");?>
