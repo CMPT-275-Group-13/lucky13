65b49ca110799d1d7eaf6517a3a7f010d64ff79f
Elaine Crisostomo <ecrisost@sfu.ca>
2019-11-18 22:09:45 -0800

    Add basic UITests to the web app

---
+++ projects/website/testing/PaperPlane.UITest/PaperPlane.UITest/PaperPlane.UITest/RegisterTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PaperPlane.UITest
+{
+    class RegisterTests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void BeforeEachTests()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://cmpt275-paperplane.cjinng.com/");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            if(driver != null)
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
