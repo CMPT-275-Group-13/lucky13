551ce3a4c35bc907edf4a711acf6133e5f6a2d46
Elaine Crisostomo <ecrisost@sfu.ca>
2019-11-20 22:56:04 -0800

    added boostrap styling to register and login page

--- projects/website/src/register.php
+++ projects/website/src/register.php
@@ -11,47 +11,60 @@
 <?php include("includes/design-top.php");?>
 <?php //include("includes/navigation.php");?>
 
-<div class="container" id="main-content">
-	<div>
-		<div class="" id="">
-			<div class="" id="register-status">
-			</div>
-			<form id="register-form" class="form-inline" action="" method="POST">
-				<div class="form-group">
-					<div>
-						<label><b>Email</b></label>
+<div class="container">
+		<div class="row">
+		  <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+			<div class="card card-signin my-5">
+			  <div class="card-body">
+				<h5 class="card-title text-center">Sign Up</h5>
+				<form class="form-signup">
+				<div class="form-row">
+					<div class="col form-group">
+						<label>First Name</label>
+						<input type="text" id="first-name" class="form-control" placeholder="First Name" required autofocus>
+							
+						</div>
+					<div class="col form-group">
+							<label>Last Name</label>
+							<input type="text" id="last-name" class="form-control" placeholder="Last Name" required autofocus>
+									
+						</div>
 					</div>
-					<div>
-						<input class="form-control" type="textbox" name="email" id="email" required/>
-					</div>
-				</div>
-				<div>
-					<label><b>Password</b></label>
-				</div>
-				<div>
-					<input class="form-control" type="password" name="password" id="password" required/>
-				</div>
-				<div>
-					<label><b>First Name</b></label>
-				</div>
-				<div>
-					<input class="form-control" type="text" name="first-name" id="first-name" required/>
-				</div>
-				<div>
-					<label><b>Last Name</b></label>
-				</div>
-				<div>
-					<input class="form-control" type="text" name="last-name" id="last-name" required/>
-				</div>
-				<div class="form-group">
-					<button id="register-btn" class="btn btn-default">Register</button>
-				</div>
-			</form>
-		</div>
-	</div>
-</div>
+				  <div class="form-label-group">
+					<label for="email">Email address</label>
+					<input type="email" id="email" class="form-control" placeholder="Email address" required autofocus>
+				  </div>
+				  <div class="form-label-group">
+						<label for="password">Password</label>
+					<input type="password" id="password" class="form-control" placeholder="Password" required>
+				  </div>
 
-<?php include("includes/footer.php");?>
+				  <div class="form-label-group">
+						<label for="phoneNumber">Phone Number</label>
+					<input type="text" id="phoneNumber" class="form-control" placeholder="Phone Number" required>
+				  </div>
+	
+				  <div class="custom-control custom-checkbox mb-3">
+					<input type="checkbox" class="custom-control-input" id="customCheck1">
+					<label class="custom-control-label" for="customCheck1">I agree to the Terms & Conditions.</label>
+				  </div>
+				  <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" id="register-btn">Register</button>
+				  <hr class="my-4">
+				  <button class="btn btn-block btn-social btn-google" onclick="googleSignin()" type="submit">
+					<i class="fab fa-google"></i> Sign in with Google</button>
+				  <div class="modal-footer">
+					<div class="options text-center text-md-right mt-1">
+						Have an account? <a href="login.php" class="blue-text">Sign In</a>
+					</div>
+				  </div>
+				</form>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+
+<!-- <?php //include("includes/footer.php");?> -->
 
 </body>
 </html>
