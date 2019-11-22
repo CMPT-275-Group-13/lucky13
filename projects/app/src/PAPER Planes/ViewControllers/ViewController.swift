66f1e26ba904b87b90dd8a19d65400b02bfd3d29
khenkelm <khenkelm@asb9700-f02.csil.sfu.ca>
2019-11-22 14:28:19 -0800

    Stabilization V 1

--- projects/app/src/PAPER Planes/ViewControllers/ViewController.swift
+++ projects/app/src/PAPER Planes/ViewControllers/ViewController.swift
@@ -3,27 +3,53 @@
 //  PAPER Planes
 //
 //  Created by Angus Kan on 2019-10-31.
-//  Worked on by: Angus Kan, Marco Liang
+//  Worked on by: Angus Kan, Marco Liang, Kiel Henkelman
 //  Copyright © 2019 Angus Kan. All rights reserved.
 //
 
 import UIKit
+import CoreMotion
+import CoreGraphics
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-
-        //Listen for Keyboard Events
-        
     }
     
-
-
-
-
+    var motionManager = CMMotionManager()
+    @IBOutlet weak var topDistance: NSLayoutConstraint!
+    @IBOutlet weak var leftDistance: NSLayoutConstraint!
+    var currentXVelocity = CGFloat()
+    var currentYVelocity = CGFloat()
+    var currentX = CGFloat()
+    var currentY = CGFloat()
+    
+    override func viewDidAppear(_ animated: Bool)
+    {
+        motionManager.deviceMotionUpdateInterval = 0.01
+        
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) {(data, error) in
+            if let myData = data
+            {
+                
+                self.currentXVelocity += CGFloat(myData.userAcceleration.x * 5)
+                self.currentYVelocity -= CGFloat(myData.userAcceleration.y * 5)
+                
+                self.currentX += self.currentXVelocity
+                self.currentY += self.currentYVelocity
+                
+                // velocity drift correction
+                self.currentXVelocity += CGFloat(-self.currentX / 500)
+                self.currentYVelocity += CGFloat(-self.currentY / 500)
+                
+                // positional drift correction
+                self.currentX += CGFloat(-self.currentX / 50)
+                self.currentY += CGFloat(-self.currentY / 50)
+                
+                self.leftDistance.constant = self.currentX + 70
+                self.topDistance.constant = self.currentY + 100
+            }
+        }
+    }
 }
-
-
