e1e7b7432d69de8f9acced0459fbe9d89b4974d1
Choong Jin Ng <jin8383@gmail.com>
2019-11-29 18:12:03 -0800

    Re-implementing index.js

--- projects/website/src/includes/javascript/index.js
+++ projects/website/src/includes/javascript/index.js
@@ -1,45 +1,52 @@
-var db = firebase.firestore();
-var welcomeMessage = document.querySelector("#welcomeMessage");
-var docRef = db.collection('doctors');
+/**
+ *  index.js
+ */
 
-displayWelcome = function() {
-   var urlParams =  new URLSearchParams(window.location.search);
-   console.log("URL " + urlParams);
-   var user = urlParams.get('index');
-   console.log(user);
-   docRef.where("uid", "==", user)
-    .get().then(function(querySnapshot) {
-        var welcomeMessageStr = '';
+$(document).ready(function() {
+    firebaseCheckAuthState();
 
-        if (!querySnapshot.empty) {
-            var docs = querySnapshot.docs[0].data();
-           
-            var title = '';
-            var firstName = '';
-            var lastName = '';
+    console.log("Document ready");
+    var userEmail = firebaseGetUserEmail();
 
-            if (docs.title) {
-                title = docs.title;
+    if (userEmail) {
+        var db = firebase.firestore();
+        var welcomeMessage = document.querySelector("#welcomeMessage");
+        var docRef = db.collection('doctors');
+
+        docRef.doc(userEmail).get(function(querySnapshot) {
+            var welcomeMessageStr = '';
+    
+            if (!querySnapshot.empty) {
+                var docs = querySnapshot.docs[0].data();
+               
+                var title = '';
+                var firstName = '';
+                var lastName = '';
+    
+                if (docs.title) {
+                    title = docs.title;
+                }
+                if (docs.firstName) {
+                    firstName = docs.firstName;
+                    console.log("Doctor's first name: " + firstName);
+                }
+                if (docs.lastName) {
+                    lastName = docs.lastName;
+                }
+    
+                welcomeMessageStr = "Welcome " + title + " " + firstName + " " + lastName;
+                welcomeMessageStr.trim();
+                welcomeMessageStr += "!";
             }
-            if (docs.firstName) {
-                firstName = docs.firstName;
-                console.log("Doctor's first name: " + firstName);
+            else {
+                console.error("User does not exist");
+                welcomeMessageStr = "Welcome Doctor!";
             }
-            if (docs.lastName) {
-                lastName = docs.lastName;
-            }
-
-            welcomeMessageStr = "Welcome " + title + " " + firstName + " " + lastName;
-            welcomeMessageStr.trim();
-            welcomeMessageStr += "!";
-        }
-        else {
-            console.error("User does not exist");
-            welcomeMessageStr = "Welcome Doctor!";
-        }
-
-        welcomeMessage.innerText = welcomeMessageStr;
-    });
-}
-
-displayWelcome();
+    
+            welcomeMessage.innerText = welcomeMessageStr;
+        });
+    }
+    else {
+        console.log("No value for user email!");
+    }
+});
