2c3c10fe01f885d610872c76e6b76c0d3dbe800b
Choong Jin Ng <jin8383@gmail.com>
2019-12-01 17:17:54 -0800

    Implemented patient profile and links to the tests

--- projects/website/src/includes/javascript/patient-profile.js
+++ projects/website/src/includes/javascript/patient-profile.js
@@ -3,5 +3,13 @@
  */
 
 $(document).ready(function() {
-    
+    var urlParams =  getURLParameters();
+    var patientEmail = validateString(urlParams.get('email'));
+
+    // Populate Patient Info and Relevant links
+    firestoreDisplayUser(patientEmail, "patient");
+    jQueryGenerateURL("profile-brain-test-result", 'brain-test-results.php', patientEmail, "Brain Test Results");
+    jQueryGenerateURL("profile-tremor-test-result", 'tremor-test-results.php', patientEmail, "Tremor Test Results");
+    jQueryGenerateURL("profile-message", 'message.php', patientEmail, "Message Patient");
+
 });
--- projects/website/src/patient-profile.php
+++ projects/website/src/patient-profile.php
@@ -15,6 +15,31 @@
 
 <div class="container" id="main-content">
 	<div>
+		<div class="" id="profile">
+			<div>
+				<h2>Patient Profile</h1>
+			</div>
+			<div id="profile-email"></div>
+			<div id="profile-first-name"></div>
+			<div id="profile-last-name"></div>
+			<div id="profile-phone"></div>
+		</div>
+	</div>
+
+	<div>
+		<div>
+			<h2>Test Results</h2>
+			<div id="profile-test-results">
+				<div id="profile-brain-test-result"></div>
+				<div id="profile-tremor-test-result"></div>
+			</div>
+		</div>
+	</div>
+
+	<div>
+		<div>
+			<h2>Message Patient</h2>
+		</div>
 	</div>
 </div>
 
